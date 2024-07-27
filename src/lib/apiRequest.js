85fd7104b2d55dc8056c8980ff9aee28ec6a9a1f
Avdhut Thorat <adthorat34@gmail.com>
2024-07-27 20:09:07 +0530

    update deployment paths

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL:"http://localhost:8800/api",
+    baseURL:"https://property-listing-website-backend-ln5y.onrender.com/api",
     withCredentials:true
 })
 
